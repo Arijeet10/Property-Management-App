0263455423fc4b5f45d59685b5a2c568f4fdb27c
Arijeet <arijeetsarkar9@gmail.com>
2021-08-14 12:14:38 +0530

    created controlled component form

--- src/components/add.jsx
+++ src/components/add.jsx
@@ -2,12 +2,27 @@ import React, { useState } from "react";
 
 function Add() {
 
+    const [propData, setPropData] = useState({
+        fullName: "",
+        desc: "",
+        size: ""
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setPropData(
+            prevData => {
+                return { ...prevData, [name]: value }
+            }
+        )
+    }
+
     return (
         <div>
             <form>
-                <input type="text" name="fullName" placeholder="Enter full name" />
-                <input type="text" name="desc" placeholder="Enter Description" />
-                <input type="text" name="size" placeholder="Enter size" />
+                <input type="text" name="fullName" value={propData.fullName} placeholder="Enter full name" onChange={handleChange} />
+                <input type="text" name="desc" value={propData.desc} placeholder="Enter Description" onChange={handleChange} />
+                <input type="text" name="size" value={propData.size} placeholder="Enter size" onChange={handleChange} />
                 <button type="submit">Add Property</button>
             </form>
         </div>
