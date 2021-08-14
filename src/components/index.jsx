364c31f9797d93b2ea5c7e1e1a36decdaf0d1b7b
Arijeet <arijeetsarkar9@gmail.com>
2021-08-14 13:18:16 +0530

    added material-ui components to view property data

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,5 @@
+import { Button, Typography } from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
 import React from "react";
 
 function Home(props) {
@@ -8,12 +10,25 @@ function Home(props) {
 
     return (
         <div>
-            <ul>
-                <li>{props.propName}</li>
-                <li>{props.desc}</li>
-                <li>{props.size}</li>
-                <button type="submit" onClick={handleDelete}>Delete</button>
-            </ul>
+            <Typography variant="h2" gutterBottom>
+                Name: {props.propName}
+            </Typography>
+            <Typography variant="h3" gutterBottom>
+                Description: {props.desc}
+            </Typography>
+            <Typography variant="h4" gutterBottom>
+                Size: {props.size}
+            </Typography>
+
+            <Button
+                variant="contained"
+                color="secondary"
+                size="large"
+                startIcon={<DeleteIcon />}
+                onClick={handleDelete}
+            >
+                Delete
+            </Button>
         </div>
     )
 }
